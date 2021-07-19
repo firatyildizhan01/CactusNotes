ed3f34e1cf895d4d5af09968d793f4d5b4ced705
FIRAT-YILDIZHAN <hanyildizfirat@hotmail.com>
2021-07-19 20:51:20 +0300

    Some changes and improvements

--- app/src/main/java/com/example/cactusnotes/MainActivity.kt
+++ app/src/main/java/com/example/cactusnotes/MainActivity.kt
@@ -2,51 +2,57 @@ package com.example.cactusnotes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.contains
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.cactusnotes.databinding.ActivityMainBinding
+
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        buttonSıgnUp.setOnClickListener {
+        //binding  = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonSignUp.setOnClickListener {
 
-            if ((emailTextInputEditText.length() > 50
-                || emailTextInputEditText.length() < 5)
-                || !emailTextInputEditText.text.toString().contains("@", ignoreCase = true)
-                || !emailTextInputEditText.text.toString().contains(".", ignoreCase = true))
+            if ((binding.emailTextInputEditText.length() > 50
+                || binding.emailTextInputEditText.length() < 5)
+                || !binding.emailTextInputEditText.text.toString().contains("@", ignoreCase = true)
+                || !binding.emailTextInputEditText.text.toString().contains(".", ignoreCase = true))
              {
-                emailTextInputEditText.error = "Email is invalid."
+                 binding.emailTextInputEditText.error = "Email is invalid."
              }
-            else if (emailTextInputEditText.text.toString() == "") {
-                emailTextInputEditText.error = "Email is required."
+            else if (binding.emailTextInputEditText.text.toString() == "") {
+                binding.emailTextInputEditText.error = "Email is required."
             }
-            if (usernameTextInputEditText.text.toString() == "") {
-                usernameTextInputEditText.error = "Username is required."
+            if (binding.usernameTextInputEditText.text.toString() == "") {
+                binding.usernameTextInputEditText.error = "Username is required."
             }
-            else if (usernameTextInputEditText.length() < 2) {
-                usernameTextInputEditText.error = "Username is too short."
+            else if (binding.usernameTextInputEditText.length() < 2) {
+                binding.usernameTextInputEditText.error = "Username is too short."
             }
-            else if (usernameTextInputEditText.length() > 20) {
-                usernameTextInputEditText.error = "Username is too long."
+            else if (binding.usernameTextInputEditText.length() > 20) {
+                binding.usernameTextInputEditText.error = "Username is too long."
             }
-            else if (!usernameTextInputEditText.text.toString().matches("^[A-Za-z0-9_.]+\$".toRegex()) )
+            else if (!binding.usernameTextInputEditText.text.toString().matches("^[A-Za-z0-9_.]+\$".toRegex()) )
             {
-                usernameTextInputEditText.error = "Username can only include a-z, 0-9 and _ characters."
+                binding.usernameTextInputEditText.error = "Username can only include a-z, 0-9 and _ characters."
             }
-            if (passwordTextInputEditText.text.toString() == "") {
-                passwordTextInputEditText.error = "Password is required."
+            if (binding.passwordTextInputEditText.text.toString() == "") {
+                binding.passwordTextInputEditText.error = "Password is required."
             }
-            else if (passwordTextInputEditText.length() < 7) {
-                passwordTextInputEditText.error = "Password is too short."
+            else if (binding.passwordTextInputEditText.length() < 7) {
+                binding.passwordTextInputEditText.error = "Password is too short."
             }
-            else if (passwordTextInputEditText.length() > 40) {
-                passwordTextInputEditText.error = "Password is too long."
+            else if (binding.passwordTextInputEditText.length() > 40) {
+                binding.passwordTextInputEditText.error = "Password is too long."
             }
-            else if (!passwordTextInputEditText.text.toString()
+            else if (!binding.passwordTextInputEditText.text.toString()
                     .matches(("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~\$^+=<>])" +
                             ".{8,20}\$").toRegex()))
             {
-                passwordTextInputEditText.error ="Password must contain one digit, one uppercase letter" +
+                binding.passwordTextInputEditText.error ="Password must contain one digit, one uppercase letter" +
                         ", one lowercase letter and one special character."
             }
         }
