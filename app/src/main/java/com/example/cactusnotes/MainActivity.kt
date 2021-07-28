d33ef28c863ab458b3d023b970798637f49f1461
Ahmet Safa Orhan <ahmetsafaorhan@gmail.com>
2021-07-28 12:00:20 +0300

    Update some strings

--- app/src/main/java/com/example/cactusnotes/MainActivity.kt
+++ app/src/main/java/com/example/cactusnotes/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.cactusnotes
 
 import android.content.Intent
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cactusnotes.databinding.ActivityMainBinding
